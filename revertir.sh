240a133ab2d6975d452c39a33705d962d82db0fb
hackingyseguridad <antonio_taboada@hotmail.es>
2023-09-25 12:34:48 +0200

    Update revertir.sh

--- revertir.sh
+++ revertir.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-echo "."
+echo 
 chmod 777 revertir.sh
 echo "Uso.: sh revertir.sh nombre_fichero.apk" 
-echo ".."
+echo 
 echo " - descomprime los arvhicos de la aplicacion movil .APK, en la carpeta /tmp/ !!!"
-echo "..."
+echo 
 cd /tmp/
 cp $1 /tmp/fichero.zip
 unzip -v fichero.zip -d /tmp/
