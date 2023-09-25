ccdfc9b41baf1586eee1a6c24b627e27a8df090b
hackingyseguridad <antonio_taboada@hotmail.es>
2023-09-25 12:12:42 +0200

    Update and rename desempaquetar.sh to desempaqueta.sh

--- desempaqueta.sh
+++ desempaqueta.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 echo
-chmod 777 desempaquetar.sh
-echo "Uso.: sh desempaquetar.sh nombrep_fichero.apk" 
+chmod 777 desempaqueta.sh
+echo "Uso.: sh desempaqueta.sh nombre_fichero.apk" 
 echo "carpeta /tmp/ para ver los ficheros una vez descomprimidos"
 echo 
 cd /tmp/
 cp $1 /tmp/fichero.zip
 unzip -v fichero.zip -d /tmp/
-
