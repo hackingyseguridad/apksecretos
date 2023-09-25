ff822b969b55e544c9c13fe7ce837b52d034b9fb
hackingyseguridad <antonio_taboada@hotmail.es>
2023-09-25 21:08:28 +0200

    Update secretos.sh

--- secretos.sh
+++ secretos.sh
@@ -8,6 +8,8 @@ grep  -Hrn "token*" /tmp
 grep  -Hrn "passwd*" /tmp
 grep  -Hrn "auth" /tmp
 grep  -Hrn "api" /tmp
+grep  -Hrn "apikey" /tmp
+grep  -Hrn "id=" /tmp
 grep  -Hrn "sql" /tmp
 grep  -Hrn "Digest" /tmp
 grep  -Hrn "email" /tmp
